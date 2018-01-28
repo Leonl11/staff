38dda0eed550dd942a17b1fdaa4a09e4712a495a
Leonl11 <leonidlenchik@gmail.com>
2018-01-28 21:57:32 +0200

    release

---
+++ preloader/preloader.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function() {
+    function showhide() {
+        $("#plogo").fadeIn(1600, function() {
+            $("#plogo").fadeOut(1400, showhide);
+        })
+    }
+    showhide();
+});
+
+
+/*$(window).on('load', function() {
+    var $preloader = $('#preloader');
+    $preloader.delay(2000).fadeOut('slow');
+});*/
