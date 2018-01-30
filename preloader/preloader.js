604ad4726517dfab0a2d42b2e3af4b10e5f15561
Leonl11 <leonidlenchik@gmail.com>
2018-01-30 23:14:02 +0200

    animation rotate

--- preloader/preloader.js
+++ preloader/preloader.js
@@ -1,14 +1,8 @@
-﻿$(document).ready(function() {
-    function showhide() {
-        $("#plogo").fadeIn(1600, function() {
-            $("#plogo").fadeOut(1400, showhide);
-        })
-    }
-    showhide();
-});
-
+﻿/*
 
 $(window).on('load', function() {
     var $preloader = $('#preloader');
     $preloader.delay(2000).fadeOut('slow');
 });
+
+*/
